61ddd4b23241b399d93b99846170c9e82e486721
Trinche <m.trinchero11@gmail.com>
2017-05-22 17:28:23 +0200

    Creating the Rest API app

--- datasetLinkedin/datasetLinkedin/urls.py
+++ datasetLinkedin/datasetLinkedin/urls.py
@@ -1,7 +1,10 @@
-from django.conf.urls import include, url
+# djangorest/urls.py
+# This is the main urls.py. It should'nt be mistaken for the urls.py in the api directory
+
+from django.conf.urls import url, include
 from django.contrib import admin
 
 urlpatterns = [
-    url(r'^dashboard/', include('dashboard.urls')),
     url(r'^admin/', admin.site.urls),
+    url(r'^', include('oauth.urls'))  # Add this line
 ]
